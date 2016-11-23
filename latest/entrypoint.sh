1db5b183f752e86667afceed4ebd36579d84c0a6
cassfras21 <github@cassfras.fr>
2016-11-23 20:10:41 +0100

    Updated JRiver MC 22 files
    1. Changed: supervisord kills mediacenter22 with a HUP signal and waits
    for 60sec.
    2. Changed: added an autoremove for packages that are not longer used.
    3. Changed: access rights for jriver folder from 700 to 755.
    4. Fixed: segfaults caused by thumbnails folder. Workaround: remove
    thumbnails folder at startup.

--- latest/entrypoint.sh
+++ latest/entrypoint.sh
@@ -5,14 +5,22 @@ set -e
 if [ "$1" = '/usr/bin/supervisord' ]; then
 
 	# Checking for dist and packages updates
-	if [ "$ENV_UPDATE" = 'yes' ]; then apt-get --yes --force-yes update && apt-get --yes --force-yes dist-upgrade; fi
-
+	if [ "$ENV_UPDATE" = 'yes' ]; then
+		apt-get --yes --force-yes update
+		apt-get --yes --force-yes dist-upgrade
+		apt-get --yes autoremove
+	fi
+	
 	# Remove tmp files
 	if [ -f /tmp/.X1-lock ]; then rm /tmp/.X1-lock; fi
 	
+	# Remove JRiver thumbnails to avoid segfaults
+	TPATH=/home/jriver/.jriver/Media\ Center\ 22/Thumbnails/
+	if [ -d "$TPATH" ]; then rm -r "$TPATH"; fi
+	
 	# Set the owner and permissions for JRiver runtime folder
 	chown -R jriver:jriver /home/jriver/.jriver
-	chmod -R 700 /home/jriver/.jriver
+	chmod -R 755 /home/jriver/.jriver
 	
 	# Set VNC password
 	su - jriver -c "x11vnc -storepasswd ${ENV_VNCPASS} ~/.vnc/passwd"
